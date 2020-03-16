c4e5b3e83e69e0e0cb089362d2dcfe4304c71cf4
J-P Nurmi <jpnurmi@gmail.com>
2020-03-16 22:26:58 +0100

    Update Gnome setup

--- .setup/gnome.sh
+++ .setup/gnome.sh
@@ -19,34 +19,36 @@ if [ ! -d ~/.local/share/themes/Materia ]; then
     rm -rf /tmp/materia-theme
 fi
 
-gnome-shell-extension-tool -e user-theme
+gnome-extensions enable user-theme
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Materia-light-compact'
-gsettings set org.gnome.desktop.interface icon-theme 'Adwaita-Blue-Folders'
-gsettings set org.gnome.shell.extensions.user-theme name 'Materia-dark-compact'
 gsettings set org.gnome.desktop.interface clock-show-seconds true
 gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.interface clock-show-weekday true
 gsettings set org.gnome.desktop.interface clock-format '24h'
+gsettings set org.gnome.desktop.interface gtk-theme 'Materia-light-compact'
+gsettings set org.gnome.desktop.interface icon-theme 'Adwaita-Blue-Folders'
 gsettings set org.gnome.desktop.interface show-battery-percentage true
+
+gsettings set org.gnome.desktop.sound event-sounds false
 
 gsettings set org.gnome.shell disable-user-extensions false
 gsettings set org.gnome.shell favorite-apps "['org.gnome.Evolution.desktop', 'chromium.desktop', 'communi.desktop', 'assistant.desktop', 'org.qt-project.qtcreator.desktop', 'code.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']"
+gsettings set org.gnome.shell.extensions.user-theme name 'Materia-dark-compact'
 
 gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 'dark'
 
-EXTENSIONS="\
-    appfolders-manager@maestroschan.fr \
-    apt-update-indicator@franglais125.gmail.com \
-    auto-move-windows@gnome-shell-extensions.gcampax.github.com \
-    clipboard-indicator@tudmotu.com \
-    cpupower@mko-sl.de \
-    drive-menu@gnome-shell-extensions.gcampax.github.com \
-    horizontal-workspaces@gnome-shell-extensions.gcampax.github.com \
-    openweather-extension@jenslody.de \
-    remove-dropdown-arrows@mpdeimos.com \
-    ShellTile@emasab.it \
-"
-for ext in $EXTENSIONS; do
-    gnome-extensions install $ext
-done
+#EXTENSIONS="\
+#    appfolders-manager@maestroschan.fr \
+#    apt-update-indicator@franglais125.gmail.com \
+#    auto-move-windows@gnome-shell-extensions.gcampax.github.com \
+#    clipboard-indicator@tudmotu.com \
+#    cpupower@mko-sl.de \
+#    drive-menu@gnome-shell-extensions.gcampax.github.com \
+#    horizontal-workspaces@gnome-shell-extensions.gcampax.github.com \
+#    openweather-extension@jenslody.de \
+#    remove-dropdown-arrows@mpdeimos.com \
+#    ShellTile@emasab.it \
+#"
+#for ext in $EXTENSIONS; do
+#    gnome-extensions install $ext
+#done
