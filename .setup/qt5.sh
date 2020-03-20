556954ff98361fa0ee5e25dc2fb1a4645a38fc0e
J-P Nurmi <jpnurmi@gmail.com>
2020-03-20 15:38:58 +0100

    qt5: exclude qtrepotools & qtqa

--- .setup/qt5.sh
+++ .setup/qt5.sh
@@ -4,5 +4,6 @@ echo "Setting up Qt 5..."
 
 if [ ! -d $PROJECT_DIR/qt5 ]; then
     git -C $PROJECT_DIR clone git://code.qt.io/qt/qt5.git
-    cd $PROJECT_DIR/qt5 && ./init-repository -f --module-subset="default,-qtwebengine"
+    cd $PROJECT_DIR/qt5 && ./init-repository -f --module-subset="default,-qtqa,-qtrepotools,-qtwebengine"
 fi
+
